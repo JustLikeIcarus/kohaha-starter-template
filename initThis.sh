6a3fcdb11987d3583a6463f8393d4f50fc339df2
Kenny Kurtz <justlikeicarus@gmail.com>
2013-11-16 01:22:01 -0500

    Initial Commit

---
+++ initThis.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+git submodule add https://github.com/kohana/database.git kohana/modules/database
+
+git submodule add https://github.com/kohana/orm.git kohana/modules/orm
+
+git submodule add https://github.com/kohana/auth.git kohana/modules/auth
+
+git submodule add https://github.com/kohana/minion.git kohana/modules/minion
+
+git submodule add https://github.com/kohana/image.git kohana/modules/image
+
+git submodule add https://github.com/JustLikeIcarus/kohana-partials.git kohana/modules/partials
+
+git submodule add https://github.com/kohana/core.git kohana/system
+
+git submodule add https://github.com/shadowhand/email.git kohana/modules/email
+
+git submodule add https://github.com/kohana/userguide.git kohana/modules/userguide
+
+git submodule add https://github.com/kohana-minion/tasks-migrations.git kohana/modules/migrations
+
+git submodule add https://github.com/JustLikeIcarus/kohana-analytics.git kohana/modules/analytics
+
+git submodule update --init --recursive
+
+
+
