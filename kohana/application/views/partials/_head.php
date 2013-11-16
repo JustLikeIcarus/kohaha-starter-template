5c8eff9bbeaccf4cfd8b6754e9de4a848900e584
Kenny <JustLikeIcarus@gmail.com>
2013-11-16 01:55:54 -0500

    Update _head.php

--- kohana/application/views/partials/_head.php
+++ kohana/application/views/partials/_head.php
@@ -1,9 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="shortcut icon" href="/favicon.png">
@@ -20,4 +18,4 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
     <![endif]-->
   </head>
-  <body>
+
