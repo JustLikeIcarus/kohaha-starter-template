5e70eabd558d5159cd6ce053f775bf12fb5e3294
Kenny <JustLikeIcarus@gmail.com>
2013-11-16 03:20:12 -0500

    Create contact.php

---
+++ kohana/application/views/home/contact.php
@@ -0,0 +1,55 @@
+<div class="page-header">
+  <h1>Contact Us</h1>
+  <p class="lead">Example placeholder</p>
+</div>
+
+<div class="row">
+  <div class="col-md-12">
+  <form class="form-horizontal">
+<div class="panel">
+
+<!-- Form Name -->
+<div class="panel-heading">Contact Form</div>
+<div class="panel-body">
+<!-- Text input-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="textinput">Name</label>  
+  <div class="col-md-5">
+  <input id="textinput" name="textinput" placeholder="John Doe" class="form-control input-md" required="" type="text">
+    
+  </div>
+</div>
+
+<!-- Prepended text-->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="prependedtext">Email</label>
+  <div class="col-md-5">
+    <div class="input-group">
+      <span class="input-group-addon">@</span>
+      <input id="prependedtext" name="prependedtext" class="form-control" placeholder="john@domain.com" required="" type="text">
+    </div>
+    
+  </div>
+</div>
+
+<!-- Textarea -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="details">Details</label>
+  <div class="col-md-4">                     
+    <textarea class="form-control" id="details" name="details">How can we help you?</textarea>
+  </div>
+</div>
+
+<!-- Button -->
+<div class="form-group">
+  <label class="col-md-4 control-label" for="submit"></label>
+  <div class="col-md-4">
+    <button id="submit" name="submit" class="btn btn-primary">Submit</button>
+  </div>
+</div>
+</div>
+</div>
+</form>
+
+  </div>
+</div>
